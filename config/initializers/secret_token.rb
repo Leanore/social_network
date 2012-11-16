190e5cea753b6de4bcf5fea795289711484ec713
Elena <alena.buracheuskaya@gmail.com>
2012-11-16 15:34:59 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sem7curs::Application.config.secret_token = 'e461d3087d2cec774438e640b9ece1cea7037e4fc34e52d9e4b46ecf6a690d6b818d73ebf9b5d4bef5dafcfffcd540ab7b154c7a273e16be4662d2e6241a895f'
